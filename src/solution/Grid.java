d94cf94b3992cef765915897597f75d1578b21fa
Jakob Engebretsen <jakobfe@ntnu.no>
2018-09-06 12:07:20 +1000

    Progress with Grid class #2

--- src/solution/Grid.java
+++ src/solution/Grid.java
@@ -1,17 +1,24 @@
 package solution;
 
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.sun.javafx.geom.Rectangle;
 
+import problem.Box;
+import problem.MovingObstacle;
 import problem.ProblemSpec;
+import problem.StaticObstacle;
 
 public class Grid {
 	
 	//run problemspec to access lists of moving boxes, moving obstacles etc.
 	public void load() {
         ProblemSpec ps = new ProblemSpec();
+        this.ps = ps;
         try {
             ps.loadProblem("input1.txt");
             ps.loadSolution("output1.txt");
@@ -24,10 +31,11 @@ public class Grid {
     }
 	
 	//fields
-	private double d;
-	private int k;
-	private ArrayList<Node> vertices = new ArrayList<>();
-	private double w;
+	private ProblemSpec ps;
+	private double d; //distance between each node
+	private int k; //The k last nodes you need to search for neighborhood
+	private ArrayList<Node> vertices = new ArrayList<>(); //Array to keep nodes
+	private static double w; //length of robot arm
 	
 	//constructor
 	public Grid(double distance, int k) {
@@ -36,57 +44,87 @@ public class Grid {
 	}
 	
 	
-	//methods
-	private String getGroundTypeOnPosition(double x, double y) {
-	String s = "";
-	
-	return s;
-	}
+	//Methods
 	
 	//Method to sample vertices
 	public void sampleGrid() {
 		int a = (int) Math.floor((1-w)/d); //How many samples for each row/columns
-		System.out.println(w);
-		int b = (int) Math.pow(a,2); //total number of samples
-		double x = w/2, y = w/2;
+		double x = w/2, y = w/2; 
+		//loop to generate nodes in grid
 		for(int i = 0; i<a; i++) {
-			for(int j = 0; i<a; j++) {
+			for(int j = 0; j<a; j++) {
 				String g = assignGroundType(x,y); //g equals ground type
-				if(!g.equals("SO")) {
-					Node n = new Node(x,y,g);
-					vertices.add(n);
+				if(!g.equals("SO")) { //do not add nodes if they are situated within a static obstacle
+					Node n = new Node(x,y,g); //calls constructor in Node class
+					vertices.add(n); //is added to the collection of nodes
 				}
 				x+=d;
 			}
-			x = w/2;
+			x = w/2; //reset x-value
 			y += d;
 			
 		}
 	}
 		
-	//Assign ground type
+	//Assign ground type for node
 	private String assignGroundType(double x, double y) {
 		String result = "";
-		
-		
+		//make a rectangle area around the node with heigh w/2 and width w/2 and x,y as center point
+		Rectangle2D.Double r = new Rectangle2D.Double(x-w/2,y-w/2,w, w); 
+		for(Box MO : ps.getMovingObstacles()) { //loop through moving obstacles
+			if(r.intersects(MO.getRect())) {
+				result += "MO";
+			}
+		}
+		for(Box MB : ps.getMovingBoxes()) { //loop through moving boxes
+			if(r.intersects(MB.getRect())) {
+				result += "MB";
+			}
+		}
+		for(StaticObstacle SO : ps.getStaticObstacles()) { //loop through static obstacles
+			if(r.intersects(SO.getRect())) {
+				result += "SO";
+			}
+		}
+		if(result.equals("")) { //did not intersect -> the node is in free space
+			result+= "FS";
+		}
+
 		return result;
 	}
 		
 	//getters
+	
+	//get number of nodes in Vertices
 	private int getNumberOfSamples() {
-		int a = (int) Math.floor((1-w)/d); //How many samples for each row/columns
-		int row;
-		int col;
-		System.out.println(w);
-		return a;
+		return vertices.size();
 
 	}
+	
+	//get length of robot arm
+	private double getLength() {
+		return w;
+	}
+	
+	//get distance between nodes
+	private double getDistance() {
+		return d;
+	}
+	
+	//get k
+
+
+	
 	
 	//main for testing and debugging
 	public static void main(String[] args) {
 		Grid g = new Grid(0.1,15);
 		g.load();
-		System.out.println(g.getNumberOfSamples());
+		g.sampleGrid();
+		System.out.println(g.vertices);
+		System.out.println(g.vertices.get(1).getNeighbours());
+
+		
 	}
 	
 	
